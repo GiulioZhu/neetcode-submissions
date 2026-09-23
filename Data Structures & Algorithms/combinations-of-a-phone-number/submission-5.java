8e030ecf4d9d0940037e09fcf7281929b3d7a2b1
Giulio Zhu <79696713+GiulioZhu@users.noreply.github.com>
2026-09-23 22:57:33 +0100

    Add: combinations-of-a-phone-number - submission-5

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<String> combinations = new ArrayList<>();
+    String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) return combinations;
+        
+        dfs(0, digits, "");
+        return combinations;
+    }
+
+    public void dfs(int index, String digits, String path) {
+        if (path.length() == digits.length()) {
+            combinations.add(path);
+            return;
+        }
+
+        for (char letter : map[digits.charAt(index) - '0'].toCharArray()) {
+            dfs(index+1, digits, path + letter);
+        }
+        
+
+    }
+}
